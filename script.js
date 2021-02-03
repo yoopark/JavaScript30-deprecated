82bf1884a8beffce9e4264b10922d17e1c846adc
yopark <yopark@student.42seoul.kr>
2021-02-03 12:30:54 +0900

    REVISE: wholeX value

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 function adaptCoordinates(event) {
 	const pct = event.clientX / window.innerWidth;
-	window.scrollTo(event.pageX * pct, event.clientY, 'smooth');
+	const wholeX = document.body.clientWidth - window.innerWidth + 100; // 정확한 수식 필요
+	window.scrollTo(wholeX * pct, 'smooth');
 }
 
 function changeProperty() {
