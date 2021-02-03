23b2f01ce650740869633736d8fd5c61a3beb89d
yopark <yopark@student.42seoul.kr>
2021-02-03 13:31:20 +0900

    REVISE: day02 grarmmer according to strict

--- day02/script.js
+++ day02/script.js
@@ -1,14 +1,14 @@
 'use strict';
 
 function changeBackground() {
-	let day = new Date();
-	x = day.getHours()
+	const now = new Date();
+	const hour = now.getHours()
 	const view = document.querySelector('html');
 	
-	if (6 <= x && x <= 18) {
-		view.style.backgroundImage = 'url(https://i.pinimg.com/originals/d8/82/e1/d882e1419d173455364b85c3bc671738.jpg)';
+	if (6 <= hour && hour <= 18) {
+		view.style.backgroundImage = `url("./images/day.jpg")`;
 	} else {
-		view.style.backgroundImage = 'url(https://mblogthumb-phinf.pstatic.net/MjAyMDAzMTdfNDYg/MDAxNTg0MzczNzk3ODQy.Ooucf2f_HpBxjTYmKvVzjsUFVf8u0f8znnZJvcexQ2cg.av6oDkX_30e8d8Ln3jMaR25p_1TW-LyUee9a56dflscg.JPEG.se___ovo/%EB%85%B8%ED%8A%B8%EB%B6%81%EB%B0%B0%EA%B2%BD%ED%99%94%EB%A9%B421.jpg?type=w800)';
+		view.style.backgroundImage = `url("./images/night.jpg")`;
 	}
 }
 
