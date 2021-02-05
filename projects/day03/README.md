2e513f7120f3884d78ed27fac7798903e64bb9d4
yopark <yopark@student.42seoul.kr>
2021-02-06 03:41:14 +0900

    Fix changing y value in index.html

--- projects/day03/README.md
+++ projects/day03/README.md
@@ -1,1 +1,9 @@
 https://blog.naver.com/retspe/220789825284?proxyReferer=https%3A%2F%2Fwww.google.com%2F
+
+
+- 시간 기능 추가
+  - 게임 완료 페이지 만들기
+- Zoom 1x->2x 는 급격한데, 9x->10x는 거기서 거기임. Bar step 수정 요함.
+- day06 보고 .json으로 문제 셋 만들고 받아오기
+  - Pass 시스템, 기타 기능 알맞게 구현하기
+- 점수 산정 방식
--- srcs/script.js
+++ srcs/script.js
@@ -3,7 +3,7 @@
 function adaptCoordinates(event) {
 	const pct = event.clientX / window.innerWidth;
 	const wholeX = document.body.clientWidth - window.innerWidth + 100; // 정확한 수식 필요
-	window.scrollTo(wholeX * pct, event.clientY, 'smooth');
+	window.scrollTo(wholeX * pct, 'smooth');
 }
 
 function changeProperty() {
